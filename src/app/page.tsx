b2c7788ea470ac8d016f85a5b265e936fa63f175
printtodayuk <printtodayuk@gmail.com>
2026-05-07 04:53:32 +0600

    Brruh2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import Footer from "@/components/Footer";
 import { Activity, Shield, Clock } from "lucide-react";
 import { getFormFields } from "@/lib/formService";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   const fields = await getFormFields();
   return (
