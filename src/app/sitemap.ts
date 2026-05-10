26e5a6d53f738accd3f1c7743c8b383bbf3954c1
printtodayuk <printtodayuk@gmail.com>
2026-05-10 16:25:52 +0600

    life sucks

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,9 +1,9 @@
 import { MetadataRoute } from 'next'
- 
+
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://medicareclinic.ca',
+      url: 'https://medicarecalgary.com',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 1.0,
